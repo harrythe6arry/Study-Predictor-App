61ba75a978efd36c429aec348520f65e44389915
harrythe6arry <voravit.sri@student.mahidol.edu>
2025-03-25 19:20:17 +0700

    add ports

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,6 +1,9 @@
 from . import create_app
+import os
 
 app = create_app()  # Create the Flask app instance
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port)
+
